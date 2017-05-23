fc7f1d7daca69fbc3b8464c8d29841827d060826
caoweiaaa <13161083183@163.com>
2017-05-23 18:43:33 +0800

    Signed-off-by: caoweiaaa <13161083183@163.com>

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,84 @@
 <template>
-  <div id="app">
-    <router-view class="view one"></router-view>
-    <div class="tab">
-    	<div class="tab_item">
-    		<router-link to="/goods" class="tab_click">商品</router-link>
-    	</div>
-    	<div class="tab_item">
-    		<router-link to="/ratings" class="tab_click">评价</router-link>
-    	</div>
-    	<div class="tab_item">
-    		<router-link to="/seller" class="tab_click">商家</router-link>
-    	</div>
-    </div>
-    <router-view class="view two" name="content"></router-view>
-  </div>
+	<div id="app">
+		<router-view class="view one"></router-view>
+		<div class="tab">
+			<div class="tab_item">
+				<router-link to="/goods" class="tab_click">商品</router-link>
+			</div>
+			<div class="tab_item">
+				<router-link to="/ratings" class="tab_click">评价</router-link>
+			</div>
+			<div class="tab_item">
+				<router-link to="/seller" class="tab_click">商家</router-link>
+			</div>
+		</div>
+		<router-view class="view two" name="content"></router-view>
+	</div>
 </template>
 
 <script>
-import axios from 'axios'
-export default {
-  name: 'app',
-  data(){
-  	return{
-  		
-  	}
-  },
-  created () {
-    axios.get('/api/seller', {
-    params: {
-    }
-  })
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-  },
-}
+	import axios from 'axios'
+	export default {
+		name: 'app',
+		data() {
+			return {
+
+			}
+		},
+		//created () {
+		//  axios.get('/api/seller', {
+		//  params: {
+		//  }
+		//})
+		//.then(function (response) {
+		//  console.log(response);
+		//})
+		//.catch(function (error) {
+		//  console.log(error);
+		//});
+		//},
+	}
 </script>
 
 <style>
 	html {
-  font-size: 62.5%;
-}
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-.tab{
-	display: flex;
-	width: 100%;
-	height: 4rem;
-	line-height: 4rem;
-	border-bottom: 0.1rem solid rgba(7,17,27,0.1);
-}
-.tab_item{
-	flex: 1;
-	text-align: center;
-}
-.tab_click {
-	display: block;
-	font-size: 1.4rem;
-	color: rgb(77,85,93);
-}
-.router-link-active{
-	color: rgb(240,20,20);
-}
+		font-size: 62.5%;
+		font-weight: 200;
+	}
+	
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		background-image: url($url+"@2x.png");
+		@media only screen and (-webkit-min-device-pixel-ratio: 3), only screen and (min-device-pixel-ratio: 3) {
+			background-image: url($url+"@3x.png");
+		}
+		;
+		/*text-align: center;*/
+		/*color: #2c3e50;*/
+	}
+	
+	.tab {
+		display: flex;
+		width: 100%;
+		height: 4rem;
+		line-height: 4rem;
+		border-bottom: 0.1rem solid rgba(7, 17, 27, 0.1);
+	}
+	
+	.tab_item {
+		flex: 1;
+		text-align: center;
+	}
+	
+	.tab_click {
+		display: block;
+		font-size: 1.4rem;
+		color: rgb(77, 85, 93);
+	}
+	
+	.router-link-active {
+		color: rgb(240, 20, 20);
+	}
 </style>
---
+++ src/components/top/Top.vue
@@ -0,0 +1,114 @@
+<template>
+	<div class="top-header">
+
+		<div class="content-wrapper">
+			<div class="avatar">
+				<img class="avatar_" :src="seller.avatar" />
+			</div>
+			
+			<div class="content">
+				<div class="title">
+					<span class="brand"></span>
+					<span class="name">{{seller.name}}</span>
+				</div>
+					<div class="descirption">
+				{{seller.description}}/{{seller.deliveryTime}}分钟送达
+			</div>
+			
+			<div v-if="seller.supports" class="support">
+				<span class="icon"></span>
+				<span class="text">{{seller.supports[0].description}}</span>
+			</div>
+			</div>
+		</div>
+		
+		<div class="bottom-wrapper"></div>
+	</div>
+</template>
+
+<script>
+	import axios from 'axios'
+	export default {
+		data() {
+			return {
+				seller: {},
+			}
+		},
+		created() {
+			let _this = this;
+			axios.get('/api/seller', {
+					params: {
+					}
+				})
+				.then(function(response) {
+					_this.seller = response.data.data;
+				})
+				.catch(function(error) {
+					console.log(error);
+				});
+		},
+	}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+	.avatar_{
+		width: 6.4rem;
+		height: 6.4rem;
+		border-radius: 0.2rem;
+	}
+	.top-header{
+		color: white;
+		background-color: #2C3E50;
+	}
+	.content-wrapper{
+		padding: 2.4rem 1.2rem 1.8rem 2.4rem;
+		font-size: 0;
+	}
+	.avatar{
+		display: inline-block;
+		vertical-align: top;
+	}
+	.content{
+		display: inline-block;
+		background-color: red;
+		margin-left: 1.6rem;
+		font-size: 1.4rem;
+	}
+	.title{
+		margin: 0.2rem 0 1.2rem 0;
+	}
+	.brand{
+		display: inline-block;
+		width: 3rem;
+		height: 1.8rem;
+		background-image: url(brand@2x.png);
+		background-size: 3rem 1.8rem;
+		background-repeat: no-repeat;
+		vertical-align: top;
+	}
+	.name{
+		font-size: 1.6rem;
+		margin-left: 0.6rem;
+		font-weight: bold;
+		line-height: 1.8rem;
+	}
+	.descirption{
+		font-size: 1.2rem;
+	}
+	.support .icon{
+		display: inline-block;
+		vertical-align: top;
+		width: 1.2rem;
+		height: 1.2rem;
+		margin-right: 0.4rem;
+		background-size: 1.2rem;
+		background-repeat: no-repeat;
+		background-image: url(decrease_1@2x.png);
+		
+	}
+	.support .text{
+		line-height: 1.2rem;
+		font-size: 1.2rem;
+	}
+</style>
